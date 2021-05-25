afa386a3b557ec73107442c96267289e3576e91b
Giuliano Neves Damasceno <giuliano131@hotmail.com>
2021-05-24 23:39:57 -0300

    finalizando parte 3

--- ReactFunctionComponents/formulario/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ReactFunctionComponents/formulario/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,9 +2,23 @@ import { Button, TextField } from '@material-ui/core';
 import React from 'react';
 
 function FormularioCadastro() {
+  let nome = '';
   return (
-    <form>
-      <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      console.log(`${nome}`);
+    }}
+    >
+      <TextField
+        onChange={(event) => {
+          nome = event.target.value;
+        }}
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
       <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
       <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth />
       <label htmlFor="promocoes">
@@ -15,7 +29,7 @@ function FormularioCadastro() {
         Novidades
         <input id="novidades" type="checkbox" />
       </label>
-      <Button id="submit" variant="contained" color="primary">
+      <Button type="submit" id="submit" variant="contained" color="primary">
         Cadastrar
       </Button>
     </form>

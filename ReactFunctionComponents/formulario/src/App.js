2ff5a55606fb7764a9442e4553d1b4236281b74c
Giuliano Neves Damasceno <giuliano131@hotmail.com>
2021-05-24 22:28:50 -0300

    Finalizando parte2

--- ReactFunctionComponents/formulario/src/App.js
+++ ReactFunctionComponents/formulario/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 
 import React, { } from 'react';
+import { Container } from '@material-ui/core';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
 
 function App() {
   return (
-    <>
+    <Container component="article" maxWidth="sm">
       <h1>Formulário de Cadastro</h1>
       <FormularioCadastro />
-    </>
+    </Container>
   );
 }
 
--- ReactFunctionComponents/formulario/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ReactFunctionComponents/formulario/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,22 +1,12 @@
-import { Button } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 import React from 'react';
 
 function FormularioCadastro() {
   return (
     <form>
-      {/* <>{  eslint-disable-next-line jsx-a11y/label-has-associated-control  }</> */}
-      <label htmlFor="nome">
-        Nome
-        <input id="nome" type="text" />
-      </label>
-      <label htmlFor="sobrenome">
-        Sobrenome
-        <input id="sobrenome" type="text" />
-      </label>
-      <label htmlFor="cpf">
-        CPF
-        <input id="cpf" type="text" />
-      </label>
+      <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
+      <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
+      <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth />
       <label htmlFor="promocoes">
         Promoções
         <input id="promocoes" type="checkbox" />
